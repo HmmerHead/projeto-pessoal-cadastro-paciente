459bb76f9bfb84d9fa56e4e1cd5f54cb8e181ed4
Guilherme A <guilherme.almeida@before.com.br>
2023-03-09 14:41:03 -0300

    Render para tratar os erros gerais

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,9 @@
 
 namespace App\Exceptions;
 
+use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -44,5 +46,20 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof Exception) 
+            return $this->mostrarErro($exception->getMessage(), Response::HTTP_NOT_FOUND);
+
+        return parent::render($request, $exception);
+    }
+
+    private function mostrarErro(string $message, int $statusCode)
+    {
+        return response()->json([
+            'message' => $message
+        ], $statusCode);
     }
 }
