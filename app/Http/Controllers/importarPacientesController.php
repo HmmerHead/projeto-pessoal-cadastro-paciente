291cffe6011b3a1b47e6b3e50dd0be23606ee736
Guilherme A <guilherme.almeida@before.com.br>
2023-03-12 20:39:40 -0300

    melhorando tratamento do import

--- app/Http/Controllers/importarPacientesController.php
+++ app/Http/Controllers/importarPacientesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Jobs\ImportacaoArquivosJob;
 use Core\UseCase\Paciente\PacienteUseCase;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use League\Csv\Reader;
 
 class importarPacientesController extends Controller
@@ -14,17 +15,23 @@ class importarPacientesController extends Controller
      */
     public function __invoke(Request $request, PacienteUseCase $paciente)
     {
-        $csv = Reader::createFromPath($request->file('file'), 'r');
+        try {
+            $csv = Reader::createFromPath($request->file('file'), 'r');
 
-        if (empty($csv->nth(1))) {
-            throw new \Exception('CSV sem os dados');
-        }
+            if (empty($csv->nth(1))) {
+                throw new \Exception('CSV sem os dados');
+            }
 
-        $csv->setHeaderOffset(0);
-        $records = $csv->getRecords();
+            $csv->setHeaderOffset(0);
+            $records = $csv->getRecords();
 
-        foreach ($records as $record) {
-            ImportacaoArquivosJob::dispatch($record, $paciente);
+            foreach ($records as $record) {
+                ImportacaoArquivosJob::dispatch($record, $paciente);
+            }
+
+            return response('Informações enviadas para processamento');
+        } catch (\Exception $th) {
+            return response('Erro no processamento: '. $th->getMessage(), Response::HTTP_REQUEST_TIMEOUT);
         }
     }
 }
